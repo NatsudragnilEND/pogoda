ea0b9adaba3996a6ebac5027313edf315b0455e9
NatsudragnilEND <nd902077@gmail.com>
2023-09-04 20:19:52 +0500

    first commit

---
+++ src/components/Week.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2 class="week-title">На неделю</h2>
+    <div class="week">
+        <WeekDay v-for="(day, idx) in getDailyWeather" :key="idx" :day="day"/>
+    </div>
+</template>
+
+<script>
+import WeekDay from './WeekDay.vue' 
+export default {
+   components: { WeekDay },
+   computed:{
+    ...mapGetters(['getDailyWeather',])
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/WeekDay.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="week">
+        <h2>Сегодня</h2>
+        <h3>28 августа</h3>
+        <img src="" alt="">
+        <span>+18°</span>
+        <span>+15°</span>
+        <span>Облачно</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['day']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
